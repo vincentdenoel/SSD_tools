4dd1a50c581ce535d20a8feb100c903120d2cd05
Vincent Denoël <vincentdenoel@gmail.com>
2025-03-07 09:35:21 -0800

    change structure of the package

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# SSD_tools
-Tools for the structural dynamic and stochastic analysis of structures
+"╭━━━┳━━━┳━━━╮")
+"┃╭━╮┃╭━╮┣╮╭╮┃")
+"┃╰━━┫╰━━╮┃┃┃┣━━┳╮╱╭╮")
+"╰━━╮┣━━╮┃┃┃┃┃╭╮┃┃╱┃┃")
+"┃╰━╯┃╰━╯┣╯╰╯┃╰╯┃╰━╯┃")
+"╰━━━┻━━━┻━━━┫╭━┻━╮╭╯")
+"╱╱╱╱╱╱╱╱╱╱╱╱┃┃╱╭━╯┃")
+"╱╱╱╱╱╱╱╱╱╱╱╱╰╯╱╰━━╯")
+
+# SSDpy
+Tools for the structural dynamic and stochastic analysis of structures.
+
+# Installation
+Regular users
+
+// Developers //
+Clone the github project on a local drive
+Move to your SSDpy environment:
+conda activate SSDpy
+
+Install the package to your venv in editable mode:
+pip install -e /Users/vincentdenoel/git/SSDpy
+
--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
 
 setup(
     name='SSDpy',
     version='0.1',
-    description='A collection of tools for the Structural and Stochastic Dynamics of systems',
-    author='SSD research group at the University of Liège',
+    description='A collection of tools from the Structural and Stochastic Dynamics',
+    author='SSD research group at the University of Liège - supervised by V. Denoël',
     author_email='ssd@uliege.be',
-    #packages=['SSDpy','SSDpy.dyn','SSDpy.dyn.integrators','SSDpy.wind'],
-    packages=['PackageName','PackageName.SubModule'],
+    packages=find_packages(),
     install_requires=['numpy', 'scipy', 'matplotlib'], # external packages as dependencies
     )
+
