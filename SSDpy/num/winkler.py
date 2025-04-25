4ec91a7f0e7a20b627ba3724667f069c42fd6912
Vincent Denoël <vincentdenoel@gmail.com>
2025-04-25 09:15:29 -0700

    Adding hinges in the winkler beam model

--- SSDpy/num/winkler.py
+++ SSDpy/num/winkler.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def BeamElasticFoundation(EI, k, L, p):
+def BeamElasticFoundation(EI, k, L, p, x_hinge=None):
     """
     Solve a beam on elastic foundation problem.
     
@@ -9,6 +9,9 @@ def BeamElasticFoundation(EI, k, L, p):
     k  : Stiffness of the elastic foundation.
     L  : Length of the beam.
     p : (array) Vector with applied distributed loads at n points uniformly spaced along the beam.
+    x_hinge : (1D array of integers) contains indices related to the position of hinges if any, otherwise None.
+              Numbering refers to the definition of the load vector p.
+              The first node is 0, the second node is 1, etc. The last node is n-1.
     
     Returns:
     x : ndarray, Positions along the beam.
@@ -23,17 +26,43 @@ def BeamElasticFoundation(EI, k, L, p):
 
     K = np.zeros((2 * n, 2 * n))
 
-    for iel in range(nel):
-        Ke = np.array([
-            [12,       6*dx,    -12,      6*dx],
-            [6*dx,     4*dx**2, -6*dx,    2*dx**2],
-            [-12,     -6*dx,     12,     -6*dx],
-            [6*dx,     2*dx**2, -6*dx,    4*dx**2]
-        ]) * (EI / dx**3)
+    if x_hinge is None:
+        for iel in range(nel):
+            Ke = np.array([
+                [12,       6*dx,    -12,      6*dx],
+                [6*dx,     4*dx**2, -6*dx,    2*dx**2],
+                [-12,     -6*dx,     12,     -6*dx],
+                [6*dx,     2*dx**2, -6*dx,    4*dx**2]
+            ]) * (EI / dx**3)
 
-        Ke += np.diag([0.5, 0, 0.5, 0]) * k * dx
-        idof = slice(2*iel, 2*iel+4)
-        K[idof, idof] += Ke
+            Ke += np.diag([0.5, 0, 0.5, 0]) * k * dx
+            idof = slice(2*iel, 2*iel+4)
+            K[idof, idof] += Ke
+    else:
+        x_hinge =np.sort(x_hinge)
+        # remove values smaller than 1 and larger than n-2
+        x_hinge = x_hinge[x_hinge > 0]
+        x_hinge = x_hinge[x_hinge < n-1]
+
+        for iel in range(nel):
+            if iel+1 not in x_hinge:
+                Ke = np.array([
+                    [12,       6*dx,    -12,      6*dx],
+                    [6*dx,     4*dx**2, -6*dx,    2*dx**2],
+                    [-12,     -6*dx,     12,     -6*dx],
+                    [6*dx,     2*dx**2, -6*dx,    4*dx**2]
+                ]) * (EI / dx**3)
+            else:
+                Ke = np.array([
+                    [3 ,       3*dx,     -3,     0],
+                    [3*dx,     3*dx**2, -3*dx,   0],
+                    [-3,      -3*dx,      3,     0],
+                    [0,           0,      0,     0]
+                ]) * (EI / dx**3)
+
+            Ke += np.diag([0.5, 0, 0.5, 0]) * k * dx
+            idof = slice(2*iel, 2*iel+4)
+            K[idof, idof] += Ke
 
     P = np.zeros(2 * n)
     P[0::2] = p
@@ -48,8 +77,13 @@ def BeamElasticFoundation(EI, k, L, p):
         idof = slice(2*iel, 2*iel+4)
         vel = V[idof]
 
-        K_left  = np.array([6*dx, 4*dx**2, -6*dx, 2*dx**2]) * (EI / dx**3)
-        K_right = np.array([6*dx, 2*dx**2, -6*dx, 4*dx**2]) * (EI / dx**3)
+        if x_hinge is None or (iel + 1) not in x_hinge:
+
+            K_left  = np.array([6*dx, 4*dx**2, -6*dx, 2*dx**2]) * (EI / dx**3)
+            K_right = np.array([6*dx, 2*dx**2, -6*dx, 4*dx**2]) * (EI / dx**3)
+        else:
+            K_left  = np.array([3, 3*dx, -3, 0]) * (EI / dx**3)
+            K_right = np.array([3*dx, 3*dx**2, -3*dx, 0]) * (EI / dx**3)
 
         M_left[iel] = -np.dot(K_left, vel)  # Use "-" for Strength of Materials conventions
         M_right[iel] = np.dot(K_right, vel)
