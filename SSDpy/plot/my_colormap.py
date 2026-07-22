76a6f9e3105c3ffc09fafe76f406b60297368bc8
Vincent Denoël <vincentdenoel@gmail.com>
2026-07-22 10:50:28 -0700

    additional colormap

--- SSDpy/plot/my_colormap.py
+++ SSDpy/plot/my_colormap.py
@@ -16,6 +16,16 @@ strbry_div = [(0, 0.125, 0.3),(0.1, 0.35, 0.4),(0.35,0.6,0.25),(0.7,0.8,0.2),
 strbry_div_map = LinearSegmentedColormap.from_list('smap', strbry_div, N = 256)
 
 
+strbry_seq = [ (1.00, 0.97, 1.00),  # very light pink-white
+    (1.00, 0.85, 1.00),  # pale pink
+    (0.95, 0.60, 0.70),  # strawberry pink
+    (0.72, 0.30, 0.58),  # raspberry
+    (0.40, 0.20, 0.50),  # purple
+    (0.20, 0.00, 0.30),  # dark purple
+]
+
+strbry_seq_map = LinearSegmentedColormap.from_list("strawberry_sequential", strbry_seq, N=256,)
+
 def get_cmap(map=1):
     """
     Set the colormap for the plot.
@@ -30,6 +40,7 @@ def get_cmap(map=1):
         new_cmap = strbry_cont_map
     elif map == 3:
         new_cmap = strbry_div_map
-    
-    return new_cmap
+    elif map == 4:
+        new_cmap = strbry_seq_map
 
+    return new_cmap
